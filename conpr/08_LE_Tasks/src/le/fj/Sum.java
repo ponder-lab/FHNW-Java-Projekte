ba4aaa2cd5c8dd15d7ed3d515ee5df8b2066c090
Benjamin Prud'homme <bprudhomme@gradcenter.cuny.edu>
2024-06-12 14:34:29 -0400

    Remove ForkJoinPool import

--- conpr/08_LE_Tasks/src/le/fj/Sum.java
+++ conpr/08_LE_Tasks/src/le/fj/Sum.java
@@ -1,7 +1,6 @@
 package le.fj;
 
 import java.util.Random;
-import java.util.concurrent.ForkJoinPool;
 import java.util.concurrent.Executors;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Callable;
