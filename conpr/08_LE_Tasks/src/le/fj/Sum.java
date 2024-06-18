eaddd56015002c56e8612c6ddd5817f444ef5a66
Benjamin Prud'homme <139664105+bgprudhomme@users.noreply.github.com>
2024-06-18 16:10:34 -0400

    Try lower threshold

--- conpr/08_LE_Tasks/src/le/fj/Sum.java
+++ conpr/08_LE_Tasks/src/le/fj/Sum.java
@@ -6,7 +6,7 @@ import java.util.concurrent.RecursiveTask;
 
 @SuppressWarnings("serial")
 public class Sum extends RecursiveTask<Integer> {
-    final int THRESHOLD = 1000000;
+    final int THRESHOLD = 100000;
     final int[] array;
     final int lo;
     final int hi;
